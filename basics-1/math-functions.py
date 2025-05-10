8080198efee7b5bfd8eaa90ceda3f700edaf0b0c
sai <152766533+saivl2@users.noreply.github.com>
2025-05-10 05:00:11 +0000

    Commit numbers

---
+++ basics-1/math-functions.py
@@ -0,0 +1,6 @@
+print(round(3.1))
+print(round(3.1234444,2))
+print(round(3.5))
+
+print(abs(-9.3))
+print(abs(-4))
---
+++ basics-1/numbers.py
@@ -0,0 +1,12 @@
+print(type(2 + 4))
+print(2 - 4)
+print(2 * 4)
+print(type(2 / 4))
+print(type(0))
+
+print(9 + .1)
+print( 2 ** 3)
+
+print(9 // 5)
+print(9 / 5)
+print(9 % 5) # remainder operator
